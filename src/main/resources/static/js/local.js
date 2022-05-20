864ccbd5f98052120cd3e0194d0d0cd5d048e68b
SCIS3-PC17\SCIS3-17 <correo>
2022-05-19 19:57:11 -0500

    Implementacion de modal y alerts con js

--- src/main/resources/static/js/local.js
+++ src/main/resources/static/js/local.js
@@ -45,7 +45,7 @@ function listar(){
                 '<td>'+
                     '<button type="button" class="btn btn-outline-danger" onclick="eliminaUsuario(\''+usuario.id+'\')"><i class="fa-solid fa-user-minus"></i></button>'+
                     '<a href="#" onclick="verModificarUsuario(\''+usuario.id+'\')" class="btn btn-outline-warning"><i class="fa-solid fa-user-pen"></i></a>'+
-                    '<a href="visualizar.html" class="btn btn-outline-info"><i class="fa-solid fa-eye"></i></a>'+
+                    '<a href="#" onclick="verUsuario(\''+usuario.id+'\')" class="btn btn-outline-info"><i class="fa-solid fa-eye"></i></a>'+
                 '</td>'+
             '</tr>';
             //}
@@ -85,6 +85,7 @@ function eliminaUsuario(id){
     .then(response => response.json())
     .then(function(data){
         listar();
+        alertas(" Se ha eliminado el usuario exitosamente!!!",2)
     })    
 }
 
@@ -118,7 +119,10 @@ function verModificarUsuario(id){
                     '<button type="button" class="btn btn-outline-warning" onclick="modificarUsuario(\''+usuario.id+'\')">Modificar</button>'+
                 '</form>';
             }
-            document.getElementById("datos").innerHTML = cadena;
+            document.getElementById("contentModal").innerHTML = cadena;
+            var myModal = new bootstrap.Modal(document.getElementById('modalUsuario'))
+            myModal.toggle();
+
     })
 }
 
@@ -138,6 +142,53 @@ async function modificarUsuario(id){
         body: JSON.stringify(jsonData)
     });
     listar();
-    document.getElementById("datos").innerHTML = "";
-    //console.log(request.text())   
+    alertas(" Se ha modificado el usuario exitosamente!!!",1)
+    document.getElementById("contentModal").innerHTML = "";
+    var myModalEl = document.getElementById('modalUsuario')
+    var modal = bootstrap.Modal.getInstance(myModalEl) //Returns a Bootstrap modal
+    modal.hide();  
+}
+
+function verUsuario(id){
+    var settings={
+        method:'GET',
+        headers:{
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+    }
+    fetch("api/users/"+id,settings)
+    .then(response => response.json())
+    .then(function(usuario){
+            var cadena='';
+            if(usuario){
+                cadena = '<div class="p-3 mb-2 bg-dark text-white">'+
+                '<h1 class="display-6"><i class="fa-solid fa-user-pen"></i> Visualizar Usuario</h1>'+
+                '</div>'+
+                '<ul class="list-group">'+
+                    '<li class="list-group-item list-group-item-dark">Nombre: '+usuario.firstName+'</li>'+
+                    '<li class="list-group-item list-group-item-dark">Apellido: '+usuario.lastName+'</li>'+
+                    '<li class="list-group-item list-group-item-dark">Email: '+usuario.email+'</li>'+
+                '</ul>';
+            }
+            document.getElementById("contentModal").innerHTML = cadena;
+            var myModal = new bootstrap.Modal(document.getElementById('modalUsuario'))
+            myModal.toggle();
+    })
+}
+
+function alertas(mensaje,tipo){
+    var color ="";
+    if(tipo == 1){ //success verde
+        color= "success"
+    }
+    else{ //danger rojo
+        color = "danger"
+    }
+    var alerta = '<div class="alert alert-warning alert-dismissible fade show" role="alert">'+
+                    '<strong><i class="fa-solid fa-triangle-exclamation"></i></strong>'+
+                    mensaje+
+                    '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>'+
+                '</div>';
+    document.getElementById("datos").innerHTML = alerta;
 }
